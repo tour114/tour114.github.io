8fc9c15b2eaee2dea3b0e6bbed6acbf96443ef3a
tour114 <130420456+tour114@users.noreply.github.com>
2023-04-11 12:31:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
-let display = document.getElementById('display');
+const display = document.querySelector('.calculator-display');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const clearButton = document.querySelector('[data-clear]');
+const equalsButton = document.querySelector('[data-equals]');
 
-function addToDisplay(value) {
-	display.value += value;
-}
+let currentValue = '';
+let previousValue = '';
+let currentOperator = null;
+let shouldResetDisplay = false;
 
-function clearDisplay() {
-	display.value = '';
-}
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (shouldResetDisplay) {
+      display.value = '';
+      shouldResetDisplay = false;
+    }
+    display.value += button.textContent;
+  });
+});
+
+operatorButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    if (currentOperator) calculate();
+    previousValue = display.value;
+    currentOperator = button.dataset.operatorType;
+    shouldResetDisplay = true;
+  });
+});
+
+equalsButton.addEventListener('click', () => {
+  if (!currentOperator) return;
+  calculate();
+});
+
+clearButton.addEventListener('click', () => {
+  currentValue = '';
+  previousValue = '';
+  currentOperator = null;
+  display.value = '';
+});
 
 function calculate() {
-	try {
-		display.value = eval(display.value);
-	} catch (error) {
-		display.value = 'Error';
-	}
+  currentValue = display.value;
+  let result;
+
+  switch (currentOperator) {
+    case 'add':
+      result = parseFloat(previousValue) + parseFloat(currentValue);
+      break;
+    case 'subtract':
+      result = parseFloat(previousValue) - parseFloat(currentValue);
+      break;
+    case 'multiply':
+      result = parseFloat(previousValue) * parseFloat(currentValue);
+      break;
+    case 'divide':
+      result = parseFloat(previousValue) / parseFloat(currentValue);
+      break;
+    default:
+      return;
+  }
+
+  display.value = result;
+  currentOperator = null;
+  shouldResetDisplay = true;
 }
+
